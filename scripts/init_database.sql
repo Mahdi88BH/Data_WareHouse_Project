e451686f8d8fa3860f22d4edc41ff7ce8201b09f
Mahdi88BH <elmahdibahadia@gmail.com>
2025-12-21 05:32:42 -0500

    init database and schema

---
+++ scripts/init_database.sql
@@ -0,0 +1,28 @@
+-- USE DataBase => Data_WareHouse_Project
+
+USE Data_WareHouse_Project;
+
+
+-- GO : Separete Batches when Working with multiple SQL Statement
+
+/* SQL SERVER Synhaxe :
+CREATE SCHEMA Bronze;
+GO
+CREATE SCHEMA Silver;
+GO
+CREATE SCHEMA Golde;
+GO
+ */
+
+-- Define Schema of Layer
+
+-- CREATE bronze Schema
+CREATE SCHEMA bronze;
+
+-- CREATE silver Schema
+CREATE SCHEMA silver;
+
+-- CREATE gold Schema
+CREATE SCHEMA gold;
+
+
